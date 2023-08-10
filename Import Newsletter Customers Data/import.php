8d0e81de8d6cb0328fa3418e9ff3c4ea81fb66a7
RohanAjudiya <75730908+rajudiya@users.noreply.github.com>
2023-08-10 12:42:06 +0530

    Add files via upload

--- Import Newsletter Customers Data/import.php
+++ Import Newsletter Customers Data/import.php
@@ -1,42 +1,32 @@
 <?php
-  ob_start();
+/**
+ * News Letter Customer Impor Script
+ * @developer Rohan Ajudiya
+ */
+ob_start();
 use Magento\Framework\App\Bootstrap;
 require __DIR__ . '/app/bootstrap.php';
-$store_id = 1;
-$csv_filepath = "subscribers.csv";
-$csv_delimiter = ',';
-$csv_enclosure = '"';
-$magento_path = __DIR__;
-
+$csv_filepath = "subscribers.csv"; // Your Csv File path
 $params = $_SERVER;
-
 $bootstrap = Bootstrap::create(BP, $params);
-
 $obj = $bootstrap->getObjectManager();
-
 $state = $obj->get('Magento\Framework\App\State');
 $state->setAreaCode('frontend');
-
 $websiteId =  $obj->get('Magento\Store\Model\StoreManagerInterface')
     ->getStore()
     ->getWebsiteId();
-
 $customerAccountManagement =$obj
     ->create('Magento\Customer\Api\AccountManagementInterface');
-
 $_subscriberFactory = $obj->create('Magento\Newsletter\Model\SubscriberFactory');
-
-
-if (($handle = fopen($csv_filepath, "r")) !== FALSE) {
+if (($handle = fopen($csv_filepath, "r")) !== FALSE) { // Read Your Csv File
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        // email Id
-        $emailId =  $data[1];
-       /* customer */
+        $emailId =  $data[1]; // email Id
+       // Fatch customer Data 
         if($customerAccountManagement->isEmailAvailable($emailId , $websiteId)){
-            echo $emailId ."\n";
+            echo $emailId."=> Imported Successfully" ."\n";
             $_subscriberFactory->create()->subscribe($emailId );
         }else{
-            echo $emailId ."Register <br/>";
+            echo $emailId ."Register"."\n";
             $customerFactory = $obj->get('Magento\Customer\Model\CustomerFactory');
             $customer=$customerFactory->create();
             $customer->setWebsiteId($websiteId);
@@ -44,7 +34,6 @@ if (($handle = fopen($csv_filepath, "r")) !== FALSE) {
             if($customer->getId()){
                 $_subscriberFactory->create()->subscribeCustomerById($customer->getId());
             }
-
         }
     }
     fclose($handle);
